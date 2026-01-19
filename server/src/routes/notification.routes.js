6f310dbb4344f2e973357949a499d8d8d1cd40e9
GENIUS-69 <galaxygaming0511@gmail.com>
2026-01-19 19:55:02 +0530

    recreation

--- server/src/routes/notification.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { registerDeviceToken } from "../controllers/notification.controller.js";
-import authMiddleware from "../middlewares/auth.middleware.js";
-
-const router = express.Router();
-
-router.post("/register", authMiddleware, registerDeviceToken);
-
-export default router;
